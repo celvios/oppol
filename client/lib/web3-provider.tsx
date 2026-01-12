3874999148b38fb64bc8e457b24484a292533251
celvios <toluking001@gmail.com>
2026-01-12 20:26:30 +0100

    Add featured wallets to show QR code and copy link options

--- client/lib/web3-provider.tsx
+++ client/lib/web3-provider.tsx
@@ -59,6 +59,10 @@ createWeb3Modal({
         'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
     ],
     excludeWalletIds: [],
+    featuredWalletIds: [
+        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+        '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
+    ],
 });
 
 interface Web3ProviderProps {
