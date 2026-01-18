aff34b713b8216ddfefdc309d09229d5e937748d
celvios <toluking001@gmail.com>
2026-01-18 18:27:49 +0100

    fix(deposit): add missing allowance method to ERC20_ABI

--- client/app/terminal/deposit/page.tsx
+++ client/app/terminal/deposit/page.tsx
@@ -13,6 +13,7 @@ import ConnectWalletModal from "@/components/wallet/ConnectWalletModal";
 const ERC20_ABI = [
     { name: 'balanceOf', type: 'function', stateMutability: 'view', inputs: [{ name: 'account', type: 'address' }], outputs: [{ name: '', type: 'uint256' }] },
     { name: 'approve', type: 'function', stateMutability: 'nonpayable', inputs: [{ name: 'spender', type: 'address' }, { name: 'amount', type: 'uint256' }], outputs: [{ name: '', type: 'bool' }] },
+    { name: 'allowance', type: 'function', stateMutability: 'view', inputs: [{ name: 'owner', type: 'address' }, { name: 'spender', type: 'address' }], outputs: [{ name: '', type: 'uint256' }] },
     { name: 'decimals', type: 'function', stateMutability: 'view', inputs: [], outputs: [{ name: '', type: 'uint8' }] },
 ];
 
