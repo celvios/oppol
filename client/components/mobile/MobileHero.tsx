801e927dc9d832efff9ae48a8ea7217c1a961ccd
celvios <toluking001@gmail.com>
2026-01-21 20:07:40 +0100

    style: increase spacing below logo

--- client/components/mobile/MobileHero.tsx
+++ client/components/mobile/MobileHero.tsx
@@ -44,7 +44,7 @@ export default function MobileHero() {
                             priority
                         />
                     </span>
-                    <span className="text-sky-400 block text-xs tracking-[0.2em] mt-1 font-mono font-normal">
+                    <span className="text-sky-400 block text-xs tracking-[0.2em] mt-4 font-mono font-normal">
                         PREDICTION MARKET
                     </span>
                 </h1>
